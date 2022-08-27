02e2e5321bf2f63691a700dc95e62b9e756b1112
eds2002 <es23jr@gmail.com>
2022-08-27 14:47:38 -0400

    cookie test

--- pages/user/dashboard.js
+++ pages/user/dashboard.js
@@ -141,7 +141,7 @@ export const getServerSideProps = async (context)=>{
       
 
       return{
-        props:{jwtDecodedUser: decode(userJWT), userAccounts: formatAccounts || null, cookies:test || 'couldnt get cookie', testcookie:data || 'couldnt get cookie'}
+        props:{jwtDecodedUser: decode(userJWT), userAccounts: formatAccounts || null, cookies:test || 'couldnt get cookie', testcookie:validCookie || 'couldnt get cookie'}
       }
     // }
     throw error
